2f1e4e7498122790f60bb969693c7b1f388e6396
mkljn <mkljn.com@gmail.com>
2019-06-27 15:05:04 +0530

    Update GeoDto.java

--- src/main/java/ip/consumer/client/GeoDto.java
+++ src/main/java/ip/consumer/client/GeoDto.java
@@ -1,17 +1,17 @@
 package ip.consumer.client;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
 
-@Entity
+//@Entity
 public class GeoDto {
 	
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
+	//@Id
+	//@GeneratedValue(strategy=GenerationType.AUTO)
+	//private Integer id;
 	private String Ip;
 	private String Country;
 	private String State;
